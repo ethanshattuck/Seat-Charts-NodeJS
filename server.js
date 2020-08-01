fab3f72b7ca337e048ff98ed72125b2a51e4642a
Ethan Shattuck <ethanshattuck44@gmail.com>
2020-08-01 13:52:33 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.listen(3000);
