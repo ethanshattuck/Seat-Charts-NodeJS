08b3d000f881da6e47aeb8fc1f402862a44d8934
Ethan Shattuck <ethanshattuck44@gmail.com>
2020-08-01 17:38:34 -0500

    Added unavailable seats

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body);
 });
 
 app.listen(3000);
